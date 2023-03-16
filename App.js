30803d6665a271910557a3dd711effa6b7fab5da
Uthaeus <romanlavery@gmail.com>
2023-03-15 20:45:27 -0600

    added linear gradient colors to app background

--- App.js
+++ App.js
@@ -1,20 +1,20 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
-import { Linear}
+
+import { StyleSheet } from "react-native";
+import { LinearGradient } from 'expo-linear-gradient';
 
 import StartGameScreen from "./screens/StartGameScreen";
 
 export default function App() {
   return (
-    <View style={styles.rootScreen}>
+    <LinearGradient colors={['#3b021f', '#ddb52f']} style={styles.rootScreen}>
       <StartGameScreen />
-    </View>
+    </LinearGradient>
   );
 }
 
 const styles = StyleSheet.create({
   rootScreen: {
     flex: 1,
-    backgroundColor: "#ddb52f"
+    
   }
 });
