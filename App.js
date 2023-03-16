457c7d7383f56ef4b064ac9eef059b9af22a109c
Uthaeus <romanlavery@gmail.com>
2023-03-15 20:38:11 -0600

    upgraded expo

--- App.js
+++ App.js
@@ -1,14 +1,20 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { Linear}
 
-import StartGameScreen from './screens/StartGameScreen';
+import StartGameScreen from "./screens/StartGameScreen";
 
 export default function App() {
   return (
-    <StartGameScreen />
+    <View style={styles.rootScreen}>
+      <StartGameScreen />
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
-  
+  rootScreen: {
+    flex: 1,
+    backgroundColor: "#ddb52f"
+  }
 });
